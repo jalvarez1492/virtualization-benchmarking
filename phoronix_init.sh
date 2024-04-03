dbf98be181f5468242996f67a494977cfc069420
Joel Alvarez <jalvarez1492@gmail.com>
2024-04-03 16:40:32 -0400

    updated naming

--- phoronix_init.sh
+++ phoronix_init.sh
@@ -11,3 +11,8 @@ apt-get update
 apt-get install php7.2 libapache2-mod-php7.2 php7.2-common php7.2-curl php7.2-mbstring php7.2-xmlrpc php7.2-mysql php7.2-gd php7.2-xml php7.2-intl php7.2-ldap php7.2-imagick php7.2-json php7.2-cli
 
 apt-get install unzip
+
+apt-get install libssl-dev
+
+apt-get install make
+
