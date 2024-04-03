c3b4820576150fa33ef5a8b5b0091e80a439ddfc
Joel Alvarez <jalvarez1492@gmail.com>
2024-04-03 14:58:38 -0400

    updated requirements

--- phoronix_init.sh
+++ phoronix_init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Installing php ... "
+echo "Installing Phoronix Requirements ... "
 
 apt-get install software-properties-common
 
@@ -9,3 +9,5 @@ add-apt-repository -y ppa:ondrej/php
 apt-get update
 
 apt-get install php7.2 libapache2-mod-php7.2 php7.2-common php7.2-curl php7.2-mbstring php7.2-xmlrpc php7.2-mysql php7.2-gd php7.2-xml php7.2-intl php7.2-ldap php7.2-imagick php7.2-json php7.2-cli
+
+apt-get install unzip
