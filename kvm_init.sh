aaa426a3ce80e7d94a771fb1fff1351c89cd8f0c
Joel Alvarez <jalvarez1492@gmail.com>
2024-04-15 15:09:52 -0400

    Update kvm_init.sh

--- kvm_init.sh
+++ kvm_init.sh
@@ -8,7 +8,7 @@ cd /var/lib/libvirt/images
 virt-customize -a bionic-server-cloudimg-amd64.img --root-password password:root
 
 # starts a VM with name kvm_benchmarking
-virt-install --name=kvm_benchmarking --os-type=Linux --vcpu=2 --ram=8192 --disk path=/var/lib/libvirt/images/bionic-server-cloudimg-amd64.img --import --os-variant=ubuntu18.04 --network bridge:virbr0 &
+virt-install --name=kvm_bmark --os-type=Linux --vcpu=2 --ram=8192 --disk path=/var/lib/libvirt/images/bionic-server-cloudimg-amd64.img --import --os-variant=ubuntu18.04 --network bridge:virbr0 &
 
 # check to see if it's running
 virsh list --all
